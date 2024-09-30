d4f0f82cf36d5162b7ad8d003c10b3a5a500e906
Bunijin <45909223+Bunijin@users.noreply.github.com>
2024-09-30 10:20:20 +0700

    Delete MiniProject directory

--- MiniProject/.pio/build/upesy_wroom/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/upesy_wroom/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/51231/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/51231/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/51231/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/51231/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
--- MiniProject/.pio/build/upesy_wroom/libb4f/Adafruit GFX Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-.pio/build/upesy_wroom/libb4f/Adafruit\ GFX\ Library/glcdfont.c.o: \
- .pio/libdeps/upesy_wroom/Adafruit\ GFX\ Library/glcdfont.c
