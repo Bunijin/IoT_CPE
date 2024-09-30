1a00e6dcb44dfd943c7ee735ee98caeccd1bab0a
Bunijin <51231.tuchpol@gmail.com>
2024-09-30 10:19:10 +0700

    Commit MiniProject

---
+++ MiniProject/.pio/build/upesy_wroom/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/upesy_wroom/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/51231/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/51231/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/51231/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/qio_qspi/include/sdkconfig.h
---
+++ MiniProject/.pio/build/upesy_wroom/lib37e/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/upesy_wroom/lib37e/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/51231/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/51231/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/51231/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
