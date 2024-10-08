0ff08b1d5da2df824cf35359496358b219c3d0e3
Bunijin <45909223+Bunijin@users.noreply.github.com>
2024-10-08 22:13:27 +0700

    Update main.cpp

--- MiniProject/src/main.cpp
+++ MiniProject/src/main.cpp
@@ -191,9 +191,9 @@ void setupWiFi()
   WiFiManager wm;
   // wm.resetSettings();
   bool res;
-  // res = wm.autoConnect();                            // auto generated AP name from chipid
-  res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-  // res = wm.autoConnect("AutoConnectAP", "password"); // password protected ap
+  // res = wm.autoConnect();                             // auto generated AP name from chipid
+  res = wm.autoConnect("AutoConnectAP");                 // anonymous ap
+  // res = wm.autoConnect("AutoConnectAP", "password");  // password protected ap
 
   if (!res)
   {
